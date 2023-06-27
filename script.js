49fdd56b4e11beba9177aaef272f7fd4502cf946
Sonu34668 <130197661+Sonu34668@users.noreply.github.com>
2023-06-27 09:55:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-function letterCombinations(input_digit) {
-  //Complete the function
+function letterCombinations(digits) {
+  if (!digits) {
+    return [];
+  }
+  
+  const mapping = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz'
+  };
+  
+  const result = [];
+  backtrack('', digits);
+  return result;
+  
+  function backtrack(combination, nextDigits) {
+    if (nextDigits.length === 0) {
+      result.push(combination);
+      return;
+    }
+    
+    const digit = nextDigits[0];
+    const letters = mapping[digit];
+    
+    for (let i = 0; i < letters.length; i++) {
+      backtrack(combination + letters[i], nextDigits.slice(1));
+    }
+  }
 }
-
 module.exports = letterCombinations;
